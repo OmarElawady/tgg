0192ea482801e952bfd2d9b7542aceed375d1dd4
Omar Elawady <omarelawady1998@gmail.com>
2019-07-09 15:22:07 +0200

    use entry point instead of script

--- setup.py
+++ setup.py
@@ -6,17 +6,20 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="tgg-omar-elawady",
     version="0.0.0",
-    author="Example Author",
+    author="Omar Elawady",
     author_email="omarelawady1998@gmail.com",
     description="git-like package",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
     packages=setuptools.find_packages(),
+    install_requires=['click'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    scripts=['scripts/tgg']
+    entry_points={
+        'console_scripts': ['tgg = tgg.funcs:main']
+      }
 )
